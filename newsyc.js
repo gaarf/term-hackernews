075aaf3efe5a09555022d3df5bbf846e5f9500b0
Adrien <gaarf@users.noreply.github.com>
2021-10-14 16:30:54 -0700

    update to official API

--- newsyc.js
+++ newsyc.js
@@ -1,51 +1,69 @@
 #!/usr/bin/env node
 
-var List = require('term-list')
-  , list = new List({ marker: '\033[36m› \033[0m', markerLength: 2 })
-  , exec = require('child_process').exec
-  , news = {};
+const List = require("term-list"),
+  list = new List({ marker: "\033[36m› \033[0m", markerLength: 2 }),
+  exec = require("child_process").exec,
+  news = {};
 
-list.on('keypress', function(key, item){
+list.on("keypress", function (key, item) {
   var url;
   switch (key.name) {
-    case 'l':
-    case 'return':
+    case "l":
+    case "return":
       url = news[item].url;
-    case 'h':
-    case 'space':
-      url = url || 'https://news.ycombinator.com/item?id='+item;
+    case "h":
+    case "space":
+      url = url || "https://news.ycombinator.com/item?id=" + item;
 
       list.stop();
-      console.log('opening %s ...', url);
+      console.log("opening %s ...", url);
       exec('open "' + url + '"');
 
-      setTimeout(function() {
+      setTimeout(function () {
         list.start();
       }, 2000);
       break;
 
-    case 'q':
-    case 'c':
-    case 'escape':
+    case "q":
+    case "c":
+    case "escape":
       list.stop();
       process.exit();
   }
 });
 
-
 console.log("Fetching news...");
 
-require("hn.js").home(function(err, items) {
-  if(err || !items.length) {
-    console.error(err || "HN is probably down :-(");
-    process.exit(1);
-    return;
-  }
+async function fetchNews() {
+  const { default: fetch } = await import("node-fetch");
+  const r = await fetch(
+    "https://hacker-news.firebaseio.com/v0/newstories.json"
+  );
+  const j = await r.json();
+  return Promise.all(
+    j.slice(0, 5).map((id) => fetch(
+      `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+    ).then((item) => item.json()))
+  );
+}
 
-  items.forEach(function(o){
-    news[o.id] = o;
-    list.add(o.id, o.title);
-  });
-
-  list.start();
-});
+fetchNews()
+  .then((items) => {
+    if (!items.length) {
+      throw new Error("HN is probably down :-(");
+    }
+    console.log(items);
+    items.forEach(function (o) {
+      news[o.id] = o;
+      list.add(o.id, o.title);
+    });
+  })
+  .then(
+    (items) => {
+      list.start();
+    },
+    (err) => {
+      console.error(err);
+      process.exit(1);
+    }
+  );
